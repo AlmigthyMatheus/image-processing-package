0925fead4510fcd0613b9f14119d3f2173cffda9
AlmightyMatheus <135282323+AlmigthyMatheus@users.noreply.github.com>
2024-10-02 19:26:32 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name='almighty-image-processing',  # Nome novo e único
+    name='almighty-image-processing',
     version='0.0.1',
     author="AlmigthyMatheus",
     description="image Processing Package using Skimage",
